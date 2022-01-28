cab673edd4b20ec2d867f1e75e0be2086521f51f
Dimitar Yosifov <dimitaryosifov@outlook.com>
2022-01-28 11:38:04 +0200

    Favorite button fix

--- src/components/BeerDisplay.js
+++ src/components/BeerDisplay.js
@@ -38,11 +38,6 @@ class BeerDisplay extends React.Component {
     render() {
         return (
             <div className="bb-item-element">
-                <div  onClick={this.favoriteButtonClicked} className="bb-element-favorite-button">
-                    <svg ref={this.favIconRef} className="bb-svg-star" width="210mm" height="297mm" version="1.1" viewBox="0 0 210 297" xmlns="http://www.w3.org/2000/svg">
-                        <path transform="rotate(.81368 -1152.9 3834)" d="m83.602 94.594-25.582-12.986-25.2 13.712 4.4448-28.343-20.829-19.73 28.329-4.5313 12.328-25.906 13.064 25.543 28.447 3.7189-20.255 20.317z" />
-                    </svg>
-                </div>
                 <div onClick={this.itemClicked} className="bb-element-container">
                     <div className="list-element-photo-container">
                         <svg className="svg-photo-display" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink">
@@ -51,6 +46,11 @@ class BeerDisplay extends React.Component {
                     </div>
                     <div className="bb-element-name">{this.props.name}</div>
                     <div className="bb-element-tagline">{this.props.tagline}</div>
+                </div>
+                <div className="bb-element-favorite-button">
+                    <svg ref={this.favIconRef} onClick={this.favoriteButtonClicked} className="bb-svg-star" width="210mm" height="297mm" version="1.1" viewBox="0 0 210 297" xmlns="http://www.w3.org/2000/svg">
+                        <path transform="rotate(.81368 -1152.9 3834)" d="m83.602 94.594-25.582-12.986-25.2 13.712 4.4448-28.343-20.829-19.73 28.329-4.5313 12.328-25.906 13.064 25.543 28.447 3.7189-20.255 20.317z" />
+                    </svg>
                 </div>
             </div>
         );
